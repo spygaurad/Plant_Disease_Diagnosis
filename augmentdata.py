9d9ec460c10e2c65a8d2e836f1709e530cf0334d
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-05-15 16:31:59 +0545

    added code which augments train data

--- augmentdata.py
+++ augmentdata.py
@@ -2,71 +2,71 @@
 
 # ------------------------------------------------------ Code to create the files and append to rows in the dataset -----------------------------------------------------------------------------
 
-import csv
-import random
-import os
-import glob
-from PIL import Image
-from torchvision import transforms
+# import csv
+# import random
+# import os
+# import glob
+# from PIL import Image
+# from torchvision import transforms
 
-# Function to apply random augmentations to an image
-def apply_augmentations(image):
-    # Define a list of transformations to apply
-    transformations = [
-        transforms.RandomRotation(45),
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomVerticalFlip(),
-        # Add more transformations as needed
-    ]
+# # Function to apply random augmentations to an image
+# def apply_augmentations(image):
+#     # Define a list of transformations to apply
+#     transformations = [
+#         transforms.RandomRotation(45),
+#         transforms.RandomHorizontalFlip(),
+#         transforms.RandomVerticalFlip(),
+#         # Add more transformations as needed
+#     ]
     
-    # Apply random transformations
-    for transform in transformations:
-        image = transform(image)
+#     # Apply random transformations
+#     for transform in transformations:
+#         image = transform(image)
     
-    return image
+#     return image
 
-# Set the paths
-dataset_path = 'Dataset/Plant_Village'
-train_csv_path = 'Dataset/Plant_Village/valid.csv'
+# # Set the paths
+# dataset_path = 'Dataset/Plant_Village'
+# train_csv_path = 'Dataset/Plant_Village/valid.csv'
 
-# Create a list to store the new rows for train.csv
-new_rows = []
+# # Create a list to store the new rows for train.csv
+# new_rows = []
 
-# Open train.csv
-with open(train_csv_path, 'r') as csv_file:
-    reader = csv.reader(csv_file)
-    for row in reader:
-        file_path = row[0]
-        class_name = row[1]
+# # Open train.csv
+# with open(train_csv_path, 'r') as csv_file:
+#     reader = csv.reader(csv_file)
+#     for row in reader:
+#         file_path = row[0]
+#         class_name = row[1]
         
-        # Open the image
-        image_path = os.path.join(file_path)
-        image = Image.open(image_path)
+#         # Open the image
+#         image_path = os.path.join(file_path)
+#         image = Image.open(image_path)
         
-        # Apply augmentations
-        augmented_image = apply_augmentations(image)
+#         # Apply augmentations
+#         augmented_image = apply_augmentations(image)
         
-        # Generate a new random filename
-        new_file_name = f'{random.randint(40001, 70000)}.jpg'
+#         # Generate a new random filename
+#         new_file_name = f'{random.randint(40001, 70000)}.jpg'
         
-        # Save the augmented image
-        save_path = os.path.join(dataset_path, class_name, new_file_name)
-        print(save_path)
-        augmented_image.save(save_path)
+#         # Save the augmented image
+#         save_path = os.path.join(dataset_path, class_name, new_file_name)
+#         print(save_path)
+#         augmented_image.save(save_path)
         
-        # Add a new row to train.csv
-        new_row = [save_path, class_name]
-        new_rows.append(new_row)
+#         # Add a new row to train.csv
+#         new_row = [save_path, class_name]
+#         new_rows.append(new_row)
 
-# Shuffle the new rows
-random.shuffle(new_rows)
+# # Shuffle the new rows
+# random.shuffle(new_rows)
 
-# Append the new rows to train.csv
-with open(train_csv_path, 'a', newline='') as csv_file:
-    writer = csv.writer(csv_file)
-    writer.writerows(new_rows)
+# # Append the new rows to train.csv
+# with open(train_csv_path, 'a', newline='') as csv_file:
+#     writer = csv.writer(csv_file)
+#     writer.writerows(new_rows)
 
-print('Data augmentation and CSV update complete.')
+# print('Data augmentation and CSV update complete.')
 
 
 
@@ -123,24 +123,24 @@ print('Data augmentation and CSV update complete.')
 # Just removes the generated files 
 
 
-# import os
+import os
 
-# base_dir = "Dataset/Plant_Village/"
+base_dir = "Dataset/Plant_Village/"
 
-# def remove_numerical_files(folder_path):
-#     files = os.listdir(folder_path)
-#     for file in files:
-#         file_path = os.path.join(folder_path, file)
-#         if os.path.isfile(file_path) and file[:-4].isdigit():
-#             os.remove(file_path)
-#             print(f"Removed file: {file_path}")
+def remove_numerical_files(folder_path):
+    files = os.listdir(folder_path)
+    for file in files:
+        file_path = os.path.join(folder_path, file)
+        if os.path.isfile(file_path) and file[:-4].isdigit():
+            os.remove(file_path)
+            print(f"Removed file: {file_path}")
 
-# def remove_numerical_files_in_folders():
-#     folders = os.listdir(base_dir)
-#     for folder in folders:
-#         folder_path = os.path.join(base_dir, folder)
-#         if os.path.isdir(folder_path):
-#             remove_numerical_files(folder_path)
+def remove_numerical_files_in_folders():
+    folders = os.listdir(base_dir)
+    for folder in folders:
+        folder_path = os.path.join(base_dir, folder)
+        if os.path.isdir(folder_path):
+            remove_numerical_files(folder_path)
 
-# # Usage example
-# remove_numerical_files_in_folders()
+# Usage example
+remove_numerical_files_in_folders()
