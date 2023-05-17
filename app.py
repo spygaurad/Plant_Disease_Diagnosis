d7e809a9f3948df4a28a46b6de18f7db509f4af8
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-05-17 17:14:40 +0545

    deploying a better performing model

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def predict():
     transform = transforms.Compose([
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
-        Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
     image_file = transform(image_file)
     image_file = np.array(image_file)
