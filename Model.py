0940ffd7b844e8c288da14a8c43d10ee9e72d5f5
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-05-19 10:34:09 +0545

    Making the model train with a lower batch size, because higher batch size caused a generalization error

--- Model.py
+++ Model.py
@@ -103,7 +103,7 @@ class Model():
         running_correct = 0.0
         counter = 0
 
-        num = random.randint(0, len(dataset)-1)
+        # num = random.randint(0, len(dataset)-1)
 
         with torch.no_grad():
             for i, (img, label) in tqdm(enumerate(dataset), total=len(dataset)):
@@ -115,21 +115,21 @@ class Model():
                 running_correct += correct.sum().item()
                 counter += 1
                 
-                if i == num:
-                    try:
-                        os.makedirs(f"saved_samples/{MODEL_NAME}", exist_ok=True)
-                    except:
-                        pass
-                    sample = random.randint(0, BATCH_SIZE//2)
-                    image = img[sample, :, :, :].cpu().numpy().transpose((1, 2, 0))
-                    image = (image * 255).astype('uint8')
-                    image = Image.fromarray(image)
-                    draw = ImageDraw.Draw(image)
-                    real_label = self.classes[label[sample].item()]
-                    pred_label = self.classes[pred[sample].item()]
-                    draw.text((image.width - 200, 0), f"Real: {real_label}", fill='red')
-                    draw.text((image.width - 200, 20), f"Predicted: {pred_label}", fill='blue')
-                    image.save(f"saved_samples/{MODEL_NAME}/{num}.jpg")
+                # if i == num:
+                #     try:
+                #         os.makedirs(f"saved_samples/{MODEL_NAME}", exist_ok=True)
+                #     except:
+                #         pass
+                #     sample = random.randint(0, BATCH_SIZE//2)
+                #     image = img[sample, :, :, :].cpu().numpy().transpose((1, 2, 0))
+                #     image = (image * 255).astype('uint8')
+                #     image = Image.fromarray(image)
+                #     draw = ImageDraw.Draw(image)
+                #     real_label = self.classes[label[sample].item()]
+                #     pred_label = self.classes[pred[sample].item()]
+                #     draw.text((image.width - 200, 0), f"Real: {real_label}", fill='red')
+                #     draw.text((image.width - 200, 20), f"Predicted: {pred_label}", fill='blue')
+                #     image.save(f"saved_samples/{MODEL_NAME}/{num}.jpg")
 
         # loss and accuracy for a complete epoch
         epoch_acc = 100. * (running_correct / (counter*BATCH_SIZE))
