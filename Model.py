9b3919de780e50a44294487ac62dc875a139f68c
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-05-22 12:22:22 +0545

    added a final linear layer to the model, to finetune it further

--- Model.py
+++ Model.py
@@ -17,7 +17,7 @@ import matplotlib.pyplot as plt
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # DEVICE = "cpu"
-BATCH_SIZE = 8
+BATCH_SIZE = 16
 MODEL_NAME = "TOMATO_LEAF_PLANTVILLAGE_EFFICIENTNET_10CLASSES_V1_6"
 
 
--- network.py
+++ network.py
@@ -5,10 +5,10 @@ import torch.nn as nn
 class EfficientNet(nn.Module):
     def __init__(self):
         super(EfficientNet, self).__init__()
-        self.model = timm.create_model('tf_efficientnetv2_b0', num_classes=64, pretrained=True)
-        self.outLayer = nn.Linear(64, 10)
+        self.model = timm.create_model('tf_efficientnetv2_b0', pretrained=True)
+        self.model.classifier[1] = nn.Linear(1280, 10)
     
-    def forward(self, x): return self.outLayer(self.model(x))
+    def forward(self, x): return self.model(x)
 
 # device = "cuda:0" if torch.cuda.is_available() else "cpu"
 # input = torch.rand(4, 3, 224, 224).to(device)
