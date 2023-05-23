1c93b3d56c68aedae302adb827cc52d45dbcde43
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-05-23 09:45:24 +0545

    loading the half trained model

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ model = Model(trained=True)
 transform = transforms.Compose([
     Resize((224, 224)),
     ToTensor(),
-    Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    # Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
 @app.route('/predict', methods=['POST'])
