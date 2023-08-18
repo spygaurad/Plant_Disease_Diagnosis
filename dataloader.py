26bfa22bde91f5eb2d10b62ff6a6f987806134ff
Saumya Bhandary <saumyabhandary22@gmail.com>
2023-08-18 10:11:39 +0545

    added the SAM module

--- dataloader.py
+++ dataloader.py
@@ -1,10 +1,13 @@
 import os
+import pandas as pd
 from torch.utils.data import DataLoader, Dataset
 from torchvision.transforms import transforms, ToTensor, RandomRotation, RandomAffine, Resize, Normalize
 from PIL import Image
+from sklearn.model_selection import train_test_split
+import torch
 
-
-
+'''
+#Dataloader for PlantVillage Dataset
 class CustomDataset(Dataset):
     def __init__(self, csv_file, transform=None):
         self.transform = transform
@@ -65,3 +68,61 @@ def get_dataloader(root_dir, batch_size):
 
 
 # train_data, val_data, test_data = get_dataloader("Dataset/Plant_Village/", batch_size=4)
+'''
+
+
+
+
+#Dataloader for New Dataset
+class CustomDataset(Dataset):
+    def __init__(self, csv_file, classes, transform=None):
+        self.transform = transform
+        self.data = []
+        self.classes = classes
+        self.inverted_classes = {v: k for k, v in self.classes.items()}
+        with open(csv_file, 'r') as f:
+            next(f)
+            for row in f:
+                file_path, label = row.split(',')
+                label = label.strip()  
+                label = [cls_label for cls_label in self.classes.values() if label in cls_label]
+                if label:
+                    label = label[0]
+                    label_idx = self.inverted_classes[label]
+                    # Convert the label to a one-hot encoded vector
+                    one_hot_label = torch.zeros(len(classes))
+                    one_hot_label[label_idx] = 1
+                    self.data.append((file_path, one_hot_label))
+                else:
+                    continue
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        file_path, label = self.data[idx]
+
+        image = Image.open(file_path).convert('RGB')
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
+
+
+def get_dataloader(batch_size, classes):
+
+    transform = transforms.Compose([
+        RandomRotation(degrees=45), 
+        RandomAffine(degrees=0, shear=10),  # Random skewness and shear up to 10 degrees
+        Resize((224, 224)),  # Resize to 224x224
+        ToTensor(),  # Convert to tensor
+        # Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+    custom_dataset = CustomDataset(csv_file='cropped_images.csv', classes=classes, transform=transform)
+    train_dataset, test_dataset = train_test_split(custom_dataset, test_size=0.1, random_state=42)
+
+    train_data_loader = DataLoader(train_dataset, batch_size=2, shuffle=True)
+    test_data_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
+
+    return train_data_loader, test_data_loader
--- network.py
+++ network.py
@@ -7,33 +7,44 @@ class EfficientNet(nn.Module):
     def __init__(self):
         super(EfficientNet, self).__init__()
         self.model = timm.create_model('tf_efficientnetv2_b0', num_classes=64, pretrained=True)
+        # for params in self.model.parameters():
+        #     params.requires_grad = False
         self.preFinal = nn.Linear(64, 32)
         self.outlayer = nn.Linear(32, 10)
         self.relu = nn.ReLU()
-        # self.model = models.efficientnet_b0(pretrained=True)
-        # self.model.classifier[1] = nn.Linear(1280, 10)
     
-    def forward(self, x): return self.outlayer(self.relu(self.preFinal(self.model(x))))
+    def forward(self, x): 
+        return self.outlayer(self.relu(self.preFinal(self.model(x))))
 
-# device = "cuda:0" if torch.cuda.is_available() else "cpu"
-# input = torch.rand(4, 3, 224, 224).to(device)
-# model = EfficientNet().to(device)
-# output = model(input)
+    def get_activations_gradient(self):
+        return self.gradients
+
+    def forward_hook(self, module, input, output):
+        self.gradients = torch.autograd.grad(output, input[0], grad_outputs=torch.ones_like(output))[0]
+
 
 
 class DomainAdaptiveNet(nn.Module):
     def __init__(self):
         super(DomainAdaptiveNet, self).__init__()
         self.effnet = EfficientNet()
-        self.effnet.load_state_dict(torch.load('saved_model/TOMATO_LEAF_PLANTVILLAGE_EFFICIENTNET_10CLASSES_V1_6_10.pth', map_location=torch.device(DEVICE)))
+        # self.effnet.load_state_dict(torch.load('saved_model/TOMATO_LEAF_PLANTVILLAGE_EFFICIENTNET_10CLASSES_V1_6_10.pth'))
         self.shared_layers = self.effnet.model
-        self.classificationLayer = nn.Sequential(self.effnet.outlayer(self.effnet.relu(self.effnet.preFinal())))
-        self.domainClassificationLayer = nn.Sequential(nn.Linear(64, 32), nn.ReLU, nn.Linear(32, 8), nn.ReLU(8, 2))
+        self.classificationLayer = nn.Sequential(self.effnet.preFinal, self.effnet.relu, self.effnet.outlayer)
+        self.domainClassificationLayer = nn.Sequential(nn.Linear(64, 32), nn.ReLU(), nn.Linear(32, 8), nn.ReLU(), nn.Linear(8, 2))
 
     
     def forward(self, x):
-        shared_features = self.effnet(x)
-        diseaseClass = self.classificationLayer(x)
-        domain = self.domainClassificationLayer(x)
+        shared_features = self.shared_layers(x)
+        diseaseClass = self.classificationLayer(shared_features)
+        domainClass = self.domainClassificationLayer(shared_features)
 
-        return shared_features, diseaseClass, domain
+        return shared_features, diseaseClass, domainClass
+
+
+
+# device = "cuda:0" if torch.cuda.is_available() else "cpu"
+# input = torch.rand(4, 3, 224, 224).to(device)
+# model = DomainAdaptiveNet().to(device)
+# sharedFeature, diseaseClass, domainClass = model(input)
+# print("Done")
